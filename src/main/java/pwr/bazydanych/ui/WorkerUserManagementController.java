c4472337f82ff153701718c62e419769de1f6e67
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-01-10 17:51:32 +0100

    Dodałem wylistowanie wynajętych filmów po zalogowaniu użytkownika i oczyszczenie po zalogowaniu innego użytkownika

--- src/main/java/pwr/bazydanych/ui/WorkerUserManagementController.java
+++ src/main/java/pwr/bazydanych/ui/WorkerUserManagementController.java
@@ -1,28 +1,73 @@
 package pwr.bazydanych.ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import pwr.bazydanych.bdanych.AdapterBazyDanych;
 import pwr.bazydanych.bdanych.User;
+import pwr.bazydanych.bdanych.WynajetyFilm;
+
+import java.util.Vector;
 
 public class WorkerUserManagementController {
-    User user;
+    private User user;
+
+    //FXML declarations
+    @FXML
+    public TableView<WynajetyFilm> tabelawynajetych;
     @FXML
     public TextField useridtextfield;
-
     @FXML
     public Button returnmovies;
+
+    @FXML
+    private TableColumn<WynajetyFilm, String> tytulColumn;
+    @FXML
+    private TableColumn<WynajetyFilm, String> dataWynajmuColumn;
+    @FXML
+    private TableColumn<WynajetyFilm, Double> kosztColumn;
+
+
+
+    @FXML
+    public void initialize() {
+        returnmovies.setDisable(true);
+        tabelawynajetych.setVisible(false);
+    }
+
+
+    private void AfterUserFoundOperations() {
+        returnmovies.setDisable(false);
+        tabelawynajetych.setVisible(true);
+        AdapterBazyDanych adb = AdapterBazyDanych.getInstance();
+        Vector<WynajetyFilm> wynajete = adb.getFilmyWynajeteBy(user.id);
+        ObservableList<WynajetyFilm> wynajetelist = FXCollections.observableArrayList(wynajete);
+        tytulColumn.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("Tytul"));
+        dataWynajmuColumn.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("dataWypozyczenia"));
+        kosztColumn.setCellValueFactory(new javafx.scene.control.cell.PropertyValueFactory<>("aktualnykoszt"));
+        tabelawynajetych.setItems(wynajetelist);
+    }
+
+    private void AfterUserNotFoundOperations() {
+        returnmovies.setDisable(true);
+        tabelawynajetych.setVisible(false);
+    }
+
 
     @FXML
     public void onZalogujCliked(MouseEvent mouseEvent) {
         AdapterBazyDanych adb = AdapterBazyDanych.getInstance();
         user = adb.getUser(useridtextfield.getText());
-        if (user == null){
+        if (user == null) {
             SimpleDialog err = new SimpleDialog("Nie znaleziono użytkownika o podanym id.");
+            AfterUserNotFoundOperations();
             return;
         }
-        returnmovies.setDisable(false);
+        AfterUserFoundOperations();
     }
 }
