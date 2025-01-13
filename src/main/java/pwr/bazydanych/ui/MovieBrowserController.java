a81d544131591a199004aab5284c5436242ee0fe
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-01-13 23:58:54 +0100

    \("_")/

--- src/main/java/pwr/bazydanych/ui/MovieBrowserController.java
+++ src/main/java/pwr/bazydanych/ui/MovieBrowserController.java
@@ -4,9 +4,11 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import pwr.bazydanych.bdanych.AdapterBazyDanych;
 import pwr.bazydanych.bdanych.Film;
 
+import java.util.Formattable;
 import java.util.Vector;
 
 
@@ -25,21 +27,35 @@ public class MovieBrowserController
     @javafx.fxml.FXML
     private TextField tytulField;
     @javafx.fxml.FXML
-    private ChoiceBox gatunek;
+    private ChoiceBox gatunek = new ChoiceBox<>();
 
     Vector<Film> filmyquery;
 
     @javafx.fxml.FXML
     public void initialize() {
         gatunek.getItems().addAll(AdapterBazyDanych.getInstance().getGenres());
+        tytulColumn.setCellValueFactory(new PropertyValueFactory<>("tytul"));
+        rezyserColumn.setCellValueFactory(new PropertyValueFactory<>("rezyser"));
+        gatunekColumn.setCellValueFactory(new PropertyValueFactory<>("gatunek"));
     }
 
     @javafx.fxml.FXML
     public void onAddButtonClick() {
-        filmyquery = AdapterBazyDanych.getInstance().getMoviesByArg(tytulField.getText(), rezyserField.getText(), gatunek.getValue().toString());
-        tableView.getItems().clear();
-        for (Film film : filmyquery) {
-            tableView.getItems().add(film);
+        String tytul = tytulField.getText();
+        String rezyser = rezyserField.getText();
+        String genre;
+        try {
+            genre = gatunek.getValue().toString();
         }
+        catch (Exception e){
+            genre = "";
+        }
+        System.out.println("SZUKAM TYTUL : " + tytul+ "Rezyser : " + rezyser + "Gatunek : " + genre);
+        filmyquery = AdapterBazyDanych.getInstance().getMoviesByArg(rezyser,tytul,genre);
+        for(Film x : filmyquery) {
+            System.out.println(x.tytul);
+        }
+        tableView.getItems().clear();
+        tableView.getItems().addAll(filmyquery);
     }
 }
