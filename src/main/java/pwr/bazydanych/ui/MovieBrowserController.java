ff31e79ab30cb6511f7d2c621ebb1b4115bc8a87
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-01-13 23:35:14 +0100

    \("_")/

--- src/main/java/pwr/bazydanych/ui/MovieBrowserController.java
+++ src/main/java/pwr/bazydanych/ui/MovieBrowserController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import pwr.bazydanych.bdanych.AdapterBazyDanych;
+import pwr.bazydanych.bdanych.Film;
+
+import java.util.Vector;
 
 
 public class MovieBrowserController
@@ -24,6 +27,8 @@ public class MovieBrowserController
     @javafx.fxml.FXML
     private ChoiceBox gatunek;
 
+    Vector<Film> filmyquery;
+
     @javafx.fxml.FXML
     public void initialize() {
         gatunek.getItems().addAll(AdapterBazyDanych.getInstance().getGatunki());
@@ -31,5 +36,10 @@ public class MovieBrowserController
 
     @javafx.fxml.FXML
     public void onAddButtonClick() {
+        filmyquery = AdapterBazyDanych.getInstance().getFilmy(tytulField.getText(), rezyserField.getText(), gatunek.getValue().toString());
+        tableView.getItems().clear();
+        for (Film film : filmyquery) {
+            tableView.getItems().add(film);
+        }
     }
 }
