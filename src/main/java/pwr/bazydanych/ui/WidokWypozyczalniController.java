c22614a3a0ee8b661b4dace1ae7293cfbc88c59e
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-01-16 19:42:38 +0100

    JeszczeNIE

--- src/main/java/pwr/bazydanych/ui/WidokWypozyczalniController.java
+++ src/main/java/pwr/bazydanych/ui/WidokWypozyczalniController.java
@@ -55,15 +55,49 @@ public class WidokWypozyczalniController
         tabelafilm.setItems(filmyobs);
     }
 
+    private void refresh_table(){
+        Lokacja selectedItem = lokacja.getSelectionModel().getSelectedItem();
+        Vector<Film> filmy = AdapterBazyDanych.getInstance().getLocationsMovies(selectedItem.getId());
+        ObservableList<Film> filmyobs = javafx.collections.FXCollections.observableArrayList(filmy);
+        tabelafilm.setItems(filmyobs);
+    }
+
     @javafx.fxml.FXML
     public void onChoiceBoxSelectionChanged() {
     }
 
     @javafx.fxml.FXML
     public void zmiencene() {
+        Film wybrane = (Film) tabelafilm.getSelectionModel().getSelectedItem();
+        if(wybrane == null){
+            SimpleDialog simpleDialog = new SimpleDialog("Wybierz film");
+        }
+        if (AdapterBazyDanych.getInstance().changePrice(wybrane.getId(), Double.parseDouble(cenafield.getText()))) {
+            SimpleDialog simpleDialog = new SimpleDialog("Cena zmieniona");
+        }
+        else {
+            SimpleDialog simpleDialog = new SimpleDialog("Nie mozna zmienic ceny");
+        }
+        refresh_table();
     }
 
     @javafx.fxml.FXML
     public void zmienilosc() {
+        Film wybrane = (Film) tabelafilm.getSelectionModel().getSelectedItem();
+        Lokacja wybranaLokacja = lokacja.getSelectionModel().getSelectedItem();
+        if (wybranaLokacja == null) {
+            SimpleDialog simpleDialog = new SimpleDialog("Wybierz lokacje");
+            return;
+        }
+        if(wybrane == null){
+            SimpleDialog simpleDialog = new SimpleDialog("Wybierz film");
+        }
+        if (AdapterBazyDanych.getInstance().changeMovieAvailability(wybrane.getId(),wybranaLokacja.getId(), Integer.parseInt(iloscfield.getText()))) {
+            SimpleDialog simpleDialog = new SimpleDialog("Ilosc zmieniona");
+        }
+        else {
+            SimpleDialog simpleDialog = new SimpleDialog("Nie mozna zmienic ilosci");
+        }
+        refresh_table();
     }
 }
